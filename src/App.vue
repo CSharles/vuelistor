086abc60424f48a3bd3fa17d0998817c51dc70a3
Carlos Garcia <carlossgarcia@outlook.com>
2021-05-02 23:06:10 -0600

    Agregado api fetch

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="dashboard">
+    <div class="selected-card">
+      <Card :character="chosenCharacter" :closed="true" />
+    </div>
+
+    <div class="options">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Card from "./components/Card";
+import { getCharacters, randomNumber } from "./utils/api";
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  data() {
+    return {
+      options: [],
+      chosenCharacter: {},
+    };
+  },
+  async beforeCreate() {
+    const characters = await getCharacters();
+    const chosenCharacter = characters[randomNumber(0, 2)];
+    this.options = characters;
+    this.chosenCharacter = chosenCharacter;
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+.dashboard {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 20px;
+  background: rgb(154, 221, 212);
+  background: linear-gradient(
+    114deg,
+    rgba(154, 221, 212, 1) 0%,
+    rgba(171, 214, 227, 1) 100%
+  );
+}
+.image-container {
+  display: flex;
+}
+.image-container img {
+  display: inline-block;
+  width: 100%;
+}
+.selected-card {
+  display: flex;
+  justify-content: center;
+}
+.options {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 2rem;
+  gap: 15px;
+  justify-items: center;
+}
+.options .card {
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="card">
+        <div class="front">
+
+            <h2>Rick Sánchez</h2>
+            
+            <picture class="image-container">
+                <img src="https://i.pinimg.com/originals/ac/51/52/ac5152b9f7f50781b2b01e35463fc4e6.jpg" alt="Rick Sánchez">
+            </picture>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Card",
+    props: ["character", "closed"]
+}
+
+</script>
+
+<style scoped>
+.card {
+    max-width: 300px;
+    position: relative;
+    transition: all ease 0.5s;
+    transform-style: preserve-3d;
+    transform: perspective(600px) rotateY(0deg);
+}
+.card .front {
+    padding: 15px;
+    border-radius: 5px;
+    box-shadow: 0 0 4px 0 rgb(0 0 0 / 8%), 0 2px 4px 0 rgb(0 0 0 / 12%);
+    border-radius: 3px;
+    background: #fff;
+    backface-visibility: hidden;
+}
+.card::before {
+    content: '';
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: #000;
+    border-radius: 5px;
+    z-index: -1;
+}
+.card.closed {
+    transform: perspective(600px) rotateY(180deg);
+}
+.card h2 {
+    text-align: center;
+    margin-bottom: 10px;
+}
+</style>
